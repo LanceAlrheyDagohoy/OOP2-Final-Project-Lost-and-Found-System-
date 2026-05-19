a6fe0e3b4d6143a00c61450a13eed9d09ce0d6ef
Clark Khent A. Suson <skhentclark@gmail.com>
2026-05-20 00:12:43 +0800

    Add Main.java

--- app/Main.java
+++ app/Main.java
@@ -1,1 +1,10 @@
+package app;
 
+import javafx.application.Application;
+import ui.CITUFindFX;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(CITUFindFX.class, args);
+    }
+}

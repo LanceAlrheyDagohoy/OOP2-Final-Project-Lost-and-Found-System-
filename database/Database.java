9cdeba228f33c29138fcfa0be4ca0d29bef76dea
Gian Matiga <232986032+GianMatiga@users.noreply.github.com>
2026-05-19 23:42:48 +0800

    feat: add MySQL database connection class

---
+++ database/Database.java
@@ -0,0 +1,31 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/dblostandfound";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("database.Database Connected!");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not found!");
+
+        } catch (SQLException e) {
+            System.out.println("Connection Error!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
